e9e30768003407a809c4fef41216bb70c48e0cc7
unknown <kampaxton88@gmail.com>
2023-02-15 14:02:39 -0500

    Initial commit

---
+++ src/BirthMonth.java
@@ -0,0 +1,16 @@
+public class BirthMonth {
+
+    public static void main(String[] args) {
+        int N = 5;
+        if (N<1 || N>12)
+        {
+            System.out.println("You enter an incorrect month value: N");
+        }
+        else
+        {
+            System.out.println("Your birth month is: " +N);
+
+
+
+    }
+}}
